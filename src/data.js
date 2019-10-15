282b1261f731169decd47c67f4ca19eec1d9ccc9
Dagothig <guillaumenoelmartel@gmail.com>
2019-10-15 17:42:53 -0400

    Added missing scenarios and progress bar.

--- src/data.js
+++ src/data.js
@@ -71,5 +71,137 @@ module.exports.scenarios = [
     }],
     img: 'https://i.imgur.com/TuMUFD9.png',
     description: "International courts have ruled that denying access to amusement parks is a human rights abuse. The prison's budget is tight and the inmates are nearly broke, but you have an idea about how to comply with this new law."
-  }
+  },
+  {
+    title: '#7: Four Elements',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1VYGyUZHYoG70BhWaix94Bh3i-ludixcs'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/a4rh3f/nifty_fifty_7_four_elements/'
+    }],
+    img: 'https://i.imgur.com/HEe5gn0.jpg',
+    description: "Deep in the heart of the sacred forest, you'll find a very special place where each of the four elements holds sway over its own area. Each area offers its own unique challenges and opportunities."
+  },
+  {
+    title: '#8: Froggy Bottom Swamp',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=19dyVbtBU1T6EECW404Z8O8HLjpSlMRvX'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/adesjh/nifty_fifty_8_froggy_bottom_swamp/'
+    }],
+    img: 'https://i.imgur.com/JyJYJUZ.png',
+    description: "When you bought the swampland around a despised old war monument, people thought you were crazy. The soft ground isn't even stable enough to dig in. But you saw the beginnings of an amusement park empire!"
+  },
+  {
+    title: '#9: Jolly Jingle Jamboree',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1TFNUHJtcMe4OySrSDUCinaTwWCMMF57W'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/aetdyw/nifty_fifty_9_jolly_jingle_jamboree/'
+    }],
+    img: 'https://i.imgur.com/myESXiw.png',
+    description: "Although initially popular, years of lackluster management has left this Christmas-themed park in disrepair and deeply in debt. If it's going to survive and become viable, it will need to be refreshed and upgraded."
+  },
+  {
+    title: '#10: Turtle Cove',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1uldaXfcmTroh7lpcsD9Wy5zifaplYFgp'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/aikz8a/nifty_fifty_10_turtle_cove/'
+    }],
+    img: 'https://i.imgur.com/GEaWYMe.png',
+    description: "This little slice of paradise would be the perfect place for a beachfront park ... except for the many sea turtle nesting sites you're absolutely forbidden from interfering with."
+  },
+  {
+    title: '#11: Milton Gardens',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1AsbiSweh9bwKCGq3OQ-IiKZ4vpX0sXLq'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/ajy75q/nifty_fifty_11_milton_gardens/'
+    }],
+    img: 'https://i.imgur.com/8Vfw92B.png',
+    description: "If you're going to turn this struggling garden into a first-rate amusement park, you're going to need to research some much more exciting rides."
+  },
+  {
+    title: '#12: The Seventh Circle',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1CEmihsbzEdBLhK8d1N3Fkrj-HXyNqj1o'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/aoegdm/nifty_fifty_12_the_seventh_circle/'
+    }],
+    img: 'https://i.imgur.com/vGmInJx.png',
+    description: "The demons here are tired of hearing screams of pain and horror. Your punishment (you know what you did) is to give them a different kind of scream to listen to for a while."
+  },
+  {
+    title: '#13: Timber Valley',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=13Lj5ukk-fjF9GjZe7XRTcRhl6qeN9HB8'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/aqti3n/nifty_fifty_13_timber_valley/'
+    }],
+    img: 'https://i.imgur.com/dcGZnTQ.png',
+    description: "You're the type who never gives up. When life gives you lemons, you make lemonade. When life gives you a fertile valley full of trees, you build rides made out of wood."
+  },
+  {
+    title: '#14: Redtail Ridge Golf Course',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1s3H4CjCjPaevPAOAcc-zKBb39nxZivv3'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/b30xij/nifty_fifty_14_redtail_ridge_golf_course/'
+    }],
+    img: 'https://i.imgur.com/sHIQCef.png',
+    description: "This golf course has not been doing well financially. It seems golf just isn't as popular as it once was in this little corner of suburbia. Maybe if you fire up the R&D department, though, you can come up with a way to turn things around."
+  },
+  {
+    title: '#15: Adventure Land presents Aqua Shores',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1EhfhCeSUxWC52uzeZEZixxMm0sBtzQ-y'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/b3n0nh/nifty_fifty_15_adventure_land_presents_aqua_shores/'
+    }],
+    img: 'https://i.imgur.com/DGPQ6sY.png',
+    description: "Adventure Land's new water park expansion is not going well. It's over budget, behind schedule, and now their contractor has gone bankrupt. You've been hired to finish building it and make it a thriving water park before the scheduled opening date."
+  },
+  {
+    title: '#16: Su Oku River Resort',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1vOCYJH19cY_Nskj_Sg7lfHG87HUFX0eA'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/cimyfr/nifty_fifty_16_su_oku_river_resort/'
+    }],
+    img: 'https://i.imgur.com/AiaEoaT.png',
+    description: "Su Oku River Resort has long been famed for its cherry blossom massage rooms and luxurious saunas. But it has fallen on hard times and needs to be upgraded with more thrilling attractions if it's going to last."
+  },
+  {
+    title: '#17: Red Sea',
+    links: [{
+      title: 'Download',
+      href: 'https://drive.google.com/open?id=1O_h3eN0LNtwYN3dVVGN6xY0jwCPeVdjC'
+    }, {
+      title: 'Post',
+      href: 'https://www.reddit.com/r/rct/comments/ckzoxu/nifty_fifty_17_red_sea/'
+    }],
+    img: 'https://i.imgur.com/fWZEQWz.png',
+    description: 'When the Red Sea was parted, you were SUPPOSED to lead your people through it to the promised land. Not build a theme park on the bottom of the sea!'
+  },
 ];
